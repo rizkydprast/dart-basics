ba1d4c0e4f384fffaf365832070e5663b2e8366e
211240001135_RIZKYDWIPRASETYO_E <riskydwi118@gmail.com>
2024-03-22 19:12:26 +0000

    string_int.dart

---
+++ dart_project/bin/tugas1/string_int.dart
@@ -0,0 +1,11 @@
+void main() {
+  // String yang akan dikonversi menjadi int
+  String stringNumber = '123';
+
+  // Konversi String menjadi int
+  int convertedNumber = int.parse(stringNumber);
+
+  // Output hasil konversi
+  print('String: $stringNumber');
+  print('Hasil konversi ke int: $convertedNumber');
+}
